1cc570d30505ff2200393c8853017cc9f7b5cfcb
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-02-24 15:01:12 -0300

    Funcionando moedas gerais

--- index.js
+++ index.js
@@ -18,13 +18,19 @@ app.get('/', (req, res) => {
 
 app.get ('/todasmoedas', async (req, res) => {
     const resultado = await todasMoedas()
-    res.json(resultado);
+    console.log(resultado)
+    res.render('diversas', {moeda: resultado})
+    // res.json(resultado);
 });
+
+
+
 
 app.post ('/moedasespecificas', async (req, res) => {
     const { moedas } = req.body
     const resultado = await moedasEspecificas(moedas)
-    res.json(resultado);
+    // res.json(resultado);
+    res.render('especificas', {moeda: resultado})
 });
 
 app.listen(3003)
